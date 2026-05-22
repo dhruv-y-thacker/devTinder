f185197f0390e79fd6dca6a5a034015f1e6a7a4d
dhruv-y-thacker <dhruvthacker0808@gmail.com>
2026-05-22 15:00:59 +0530

    route handlers and middlewares

--- src/app.js
+++ src/app.js
@@ -2,12 +2,37 @@ const express = require('express')
 
 app = express();
 
-app.use('/',(req,resp)=>{
-    resp.send("hellow and welcome to out server and this is home route")
+const { adminAuth, userAuth } = require('../middlewares/auth.js')
+
+
+
+app.use('/user/login', (req,res)=>{
+    res.send("no user auth middleware required thats why decalred avove --app.use('/user', userAuth)-- it as express will go line by line for route matching")
 })
 
-app.use('/nam',(req,resp)=>{
-    resp.send("Namastebhailog")
+app.use('/admin', adminAuth)
+
+app.use('/user', userAuth)
+
+app.use('/nam',(req,res,next)=>{
+        // res.send("Namastebhailog")
+        console.log("came here 1")
+        next()
+    },
+    (req,res)=>{
+        console.log("came here 2")
+        res.send("namaste bahilog");
+    }
+)
+
+app.use('/admin/getAllData', (req,res)=>{
+    console.log("aata future mein ruk... middleware ka chakkar laga ke aata")
+    res.send("after adminAuth only it came here brooo so he is admin only")
+})
+
+
+app.use('/user/getData',(req,res)=> {
+    res.send ("after middleware came here");
 })
 
 app.listen(3000);
